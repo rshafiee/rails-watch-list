b535bf6a853581d29d1eca53442820f28f2916f9
Rahman Shafiee <r.shafiee@gmx.de>
2021-04-20 17:38:27 +0200

    List and Bookmark views are working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,11 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+  before_action :set_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -18,6 +19,11 @@ class ListsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
